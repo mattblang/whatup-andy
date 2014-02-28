8346cbefd669d47a8e74b24e4e764d3173049011
jlittle576 <jlittle576@gmail.com>
2014-02-27 22:30:13 -0500

    lesson1

--- whatupandy/src/main/java/com/mattblang/whatupandy/MainActivity.java
+++ whatupandy/src/main/java/com/mattblang/whatupandy/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +12,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.ic_launcher);
+        ImageView imageView2 = new ImageView(this);
+        imageView2.setImageResource(R.drawable.ic_launcher);
+
+
+        this.setTitle(R.string.hello_world);
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        this.setTitle("Hey we resumed");
     }
 }
